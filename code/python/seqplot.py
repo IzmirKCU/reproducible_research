676d1f15dca3b632608e211f622bd2f4fbadedb4
simon-rayner <simon.rayner@medisin.uio.no>
2025-09-18 09:27:09 +0200

    updated sns plot commands

--- code/python/seqplot.py
+++ code/python/seqplot.py
@@ -1,7 +1,7 @@
 from Bio import SeqIO
 import pandas as pd
 
-records = list(SeqIO.parse("/Users/simonray/uio_dropbox_sr/myTeaching/IKCU2/data/hairpin.fa", "fasta"))
+records = list(SeqIO.parse("/media/simonray/data24/data/uio_dropbox_sr/myTeaching/IKCU/reproducible_research/data/mirbase/22.1/mature.fa", "fasta"))
 
 seqLens = []
 i=0
@@ -12,4 +12,7 @@ dfseqLens = pd.DataFrame(seqLens, columns=['lengths'])
 
 from matplotlib import pyplot as plt
 import seaborn as sns
-sns.histplot(data=dfseqLens, x="lengths", binwidth=1)
+histplot=sns.histplot(data=dfseqLens, x="lengths", binwidth=1).set_title("mature lengths v22.1")
+fig=histplot.get_figure()
+fig.savefig("/media/simonray/data24/data/uio_dropbox_sr/myTeaching/IKCU/reproducible_research/data/mirbase/22.1//out.png")
+
